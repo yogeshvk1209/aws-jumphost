c010a2ef32f558e07064e2eabb6aed69f73f6191
yogeshvk1209 <yogeshvk1209@gmail.com>
2025-06-04 15:19:48 +0530

    Updating docker run cli

--- userdata.sh
+++ userdata.sh
@@ -7,5 +7,5 @@ yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/has
 yum -y install terraform git docker python3-pip
 systemctl enable docker
 systemctl start docker
-mkdir -p /home/jenkins && chmod +R 777 /home/jenkins
-docker run -dit -p 8080:8080 -p 50000:50000 -v /home/jenkins:/var/jenkins_home yogeshvk1209/jenkins2.504_jdk21:latest
+mkdir -p /home/jenkins && chmod -R 777 /home/jenkins
+docker run -dit -p 8080:8080 -p 50000:50000 -v /home/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --group-add $(getent group docker | cut -d: -f3) yogeshvk1209/jenkins2.504_jdk21

afae8bbcad3db2a76a55aa6be0caa86867a3c32c
yogeshvk1209 <yogeshvk1209@gmail.com>
2025-07-22 12:51:34 +0530

    Adding kube autocompletion

--- userdata.sh
+++ userdata.sh
@@ -4,7 +4,11 @@ exec >> $log_file 2>&1
 yum update -y
 yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-yum -y install terraform git docker python3-pip
+yum -y install terraform git docker python3-pip bash-completion
+# Enable bash-completion system-wide
+if [ -f /etc/profile.d/bash_completion.sh ]; then
+  source /etc/profile.d/bash_completion.sh
+fi
 #Docker Config
 systemctl enable docker
 systemctl start docker
@@ -14,3 +18,19 @@ docker run -dit -p 8080:8080 -p 50000:50000 -v /home/jenkins:/var/jenkins_home -
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/
+# kubectl bash completion setup
+kubectl completion bash > /etc/bash_completion.d/kubectl
+# Add kubectl completion to /etc/bashrc for all users
+if ! grep -q 'source <(kubectl completion bash)' /etc/bashrc; then
+  echo 'source <(kubectl completion bash)' >> /etc/bashrc
+fi
+# Add kubectl completion to root's .bashrc
+if ! grep -q 'source <(kubectl completion bash)' /root/.bashrc; then
+  echo 'source <(kubectl completion bash)' >> /root/.bashrc
+fi
+# Add kubectl completion to jenkins user if home exists
+if [ -d /home/jenkins ]; then
+  if ! grep -q 'source <(kubectl completion bash)' /home/jenkins/.bashrc 2>/dev/null; then
+    echo 'source <(kubectl completion bash)' >> /home/jenkins/.bashrc
+  fi
+fi

4b1e08c8db473629a5d0ec5efba3c9e2ff831398
yogeshvk1209 <yogeshvk1209@gmail.com>
2025-08-02 15:39:06 +0530

    Userdata update

--- userdata_ollama.sh
+++ userdata_ollama.sh
@@ -13,5 +13,5 @@ systemctl enable docker
 systemctl start docker
 ## Ollama related setup
 docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama --restart always ollama/ollama
-docker exec -it ollama ollama pull gemma3:1b
+docker exec ollama ollama pull gemma3:1b
 docker run -d -p 8080:8080 --add-host=host.docker.internal:host-gateway -v ollama-webui:/app/backend/data --name ollama-webui --restart always ghcr.io/ollama-webui/ollama-webui:main
